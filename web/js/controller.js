c388affd3d8b2f371b485e7a1e51ba20f51f658c
michael.congreve <michael.congreve@holmesmedia.co.uk>
2015-06-29 14:55:51 +0100

    Improve Feedlist service

--- web/js/controller.js
+++ web/js/controller.js
@@ -10,28 +10,49 @@ rssApp.controller('feedController', ["$scope", 'feedService', function ($scope, 
         $scope.feed = function() { return feedService.getFeed(); };
 }]);
 
-rssApp.controller('feedListController', ["$scope", "$http", "feedListService", "feedService", function($scope, $http, feedListService, feedService) {
-    
-    $scope.feedList = feedListService.query('/getFeedList');
-      
+rssApp.controller('feedListController', ["$scope", "feedListService", "feedService", function($scope, feedListService, feedService) {
+
+    feedListService.setFeedList(feedListService.loadFeedList());    
+    $scope.feedList = function() { return feedListService.getFeedList(); };
+
+
     $scope.addFeed = function() {
         $scope.feedlist.list.push({name:$scope.feedlist.newUrl, title:$scope.feedlist.newUrl});
         $scope.feedlist.newUrl = '';
     };
     
     $scope.load = function(feedName) {
-        feedService.setFeed(feedService.loadFeed(feedName));        
+        feedService.setFeed(feedService.loadFeed(feedName));
     };
      
 }]);
 
 rssApp.factory('feedListService', ["$resource", function($resource) {     
-        return $resource('/getFeedList', {}, {query: {isArray:true}});    
+        
+        var feedList = [];
+        
+        return {
+            addFeed: function(feedUrl) {
+                
+            },
+            deleteFeed: function(feedUrl) {
+                
+            },
+            loadFeedList: function() {
+                return $resource('/getFeedList', {}).query({isArray:true});
+            },
+            setFeedList: function(newList) {              
+                feedList = newList;               
+            },
+            getFeedList: function() {   
+                return feedList;
+            }
+        };
 }]);
 
 rssApp.service('feedService', ["$resource", function($resource) {
        
-    var feed = {info:{title:""}};    
+    var feed = {info:{title:""}};
     
     return {
         loadFeed: function(feedUrl) {
